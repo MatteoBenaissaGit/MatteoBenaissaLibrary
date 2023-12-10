2d874e021defb162c94ca9b970605841f502470d
Mattéo Benaissa <92030744+MatteoBenaissaGit@users.noreply.github.com>
2023-12-10 18:49:49 +0100

    ragdoll base

---
+++ Assets/MatteoBenaissaLibrary/Ragdoll/RagdollController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatteoBenaissaLibrary.Ragdoll
+{
+    public class RagdollController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _characterToCopy;
+        [SerializeField] private GameObject _characterRagdoll;
+        [SerializeField] private List<RagdollLimb> _limbs = new List<RagdollLimb>();
+
+        public void CopyLimbs()
+        {
+            _characterToCopy.SetActive(false);
+            _characterRagdoll.SetActive(true);
+            
+            _limbs.ForEach(x => x.CopyBaseLimb());
+        }
+    }
+}
---
+++ Assets/MatteoBenaissaLibrary/Ragdoll/RagdollLimb.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace MatteoBenaissaLibrary.Ragdoll
+{
+    public class RagdollLimb : MonoBehaviour
+    {
+        [SerializeField] private Transform _limbToCopy;
+
+        public void CopyBaseLimb()
+        {
+            transform.position = _limbToCopy.position;
+            transform.rotation = _limbToCopy.rotation;
+            transform.localScale = _limbToCopy.localScale;
+        }
+    }
+}
